ed9debc3d72652849c35553d59c8050e464fbf43
YakovSalman <106259638+YakovSalman@users.noreply.github.com>
2022-10-31 20:11:55 +0200

    Add files via upload

---
+++ assets/app/app.js
@@ -0,0 +1,57 @@
+const body = document.querySelector('#body');
+const header = document.querySelector('#header');
+const menu = document.querySelector('#menu');
+const nav = document.querySelector('#nav');
+const navLink = document.querySelectorAll('.nav__link');
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    menu.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        menu.classList.toggle('active');
+        body.classList.toggle('active');
+        nav.classList.toggle('active');
+    });
+
+
+
+
+    function FixedHeader() {
+        const headerH = header.clientHeight;
+        document.body.style.paddingTop = headerH + 'px';
+        if (window.scrollY > headerH) {
+            header.classList.add('header__fixed');
+        } else {
+            header.classList.remove('header__fixed');
+            document.body.style.paddingTop = 0;
+        }
+    }
+
+    window.addEventListener('scroll', FixedHeader);
+
+
+    for(let item of navLink) {
+        item.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            let href = e.target.getAttribute("href").substring(1);
+            const scrollTarget = document.getElementById(href);
+
+            const topOffset = document.querySelector(".header").offsetHeight;
+            const elementPosition = scrollTarget.getBoundingClientRect().top;
+            const offsetPosition = elementPosition - topOffset;
+
+            window.scrollBy({
+                top: offsetPosition,
+                behavior: "smooth",
+            });
+
+
+            menu.classList.remove('active');
+            body.classList.remove('active');
+            nav.classList.remove('active');
+        });
+    }
+});
